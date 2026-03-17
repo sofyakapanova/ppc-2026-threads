a2a0b7a4434db08cf75e60cebddb272439ded9c9
Sveta-ls <slana6570@gmail.com>
2026-03-17 21:39:56 +0000

    clang-format7

--- tasks/zyazeva_s_matrix_mult_cannon_alg/omp/include/ops_omp.hpp
+++ tasks/zyazeva_s_matrix_mult_cannon_alg/omp/include/ops_omp.hpp
@@ -5,14 +5,14 @@
 
 namespace zyazeva_s_matrix_mult_cannon_alg {
 
-std::vector<double> CannonMatrixMultiplication(const std::vector<double>& a, const std::vector<double>& b, int n);
+std::vector<double> CannonMatrixMultiplication(const std::vector<double> &a, const std::vector<double> &b, int n);
 
 class ZyazevaSMatrixMultCannonAlgOMP : public BaseTask {
  public:
   static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
     return ppc::task::TypeOfTask::kOMP;
   }
-  explicit ZyazevaSMatrixMultCannonAlgOMP(const InType& in);
+  explicit ZyazevaSMatrixMultCannonAlgOMP(const InType &in);
 
  private:
   bool ValidationImpl() override;
