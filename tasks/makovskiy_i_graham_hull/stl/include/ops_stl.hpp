d847763a935fc16ca643beac41c9a4b06b55ec32
Makoi66 <makoi199563@gmail.com>
2026-04-04 07:59:27 +0000

    stl

---
+++ tasks/makovskiy_i_graham_hull/stl/include/ops_stl.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "makovskiy_i_graham_hull/common/include/common.hpp"
+
+namespace makovskiy_i_graham_hull {
+
+class ConvexHullGrahamSTL : public BaseTask {
+ public:
+  static constexpr ppc::task::TypeOfTask GetStaticTypeOfTask() {
+    return ppc::task::TypeOfTask::kSTL;
+  }
+  explicit ConvexHullGrahamSTL(const InType &in);
+
+ private:
+  bool ValidationImpl() override;
+  bool PreProcessingImpl() override;
+  bool RunImpl() override;
+  bool PostProcessingImpl() override;
+};
+
+}  // namespace makovskiy_i_graham_hull
